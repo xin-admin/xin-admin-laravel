4a033cddde7de6526cc01cdb199d532035b82a13
liu <2302563948@qq.com>
2025-03-11 18:53:23 +0800

    refactor: 删除未使用的引用

--- app/Http/Admin/Controllers/IndexController.php
+++ app/Http/Admin/Controllers/IndexController.php
@@ -3,18 +3,51 @@
 namespace App\Http\Admin\Controllers;
 
 use App\Attribute\route\GetMapping;
+use App\Attribute\route\PostMapping;
 use App\Attribute\route\RequestMapping;
 use App\Http\BaseController;
+use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\StreamedEvent;
+use Illuminate\Support\Facades\Log;
+use OpenAI\Laravel\Facades\OpenAI;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 #[RequestMapping('/admin')]
 class IndexController extends BaseController
 {
+    protected array $noPermission = ['index', 'openai'];
+
     /** 测试接口 */
     #[GetMapping]
     public function index(): JsonResponse
     {
         $webSetting = get_setting('web');
         return $this->success(compact('webSetting'), '恭喜你已经成功安装 Xin Admin');
+    }
+
+    #[GetMapping('/openai')]
+    public function openai(): JsonResponse
+    {
+        return $this->success(OpenAI::models()->list()->toArray());
+    }
+
+    #[PostMapping('/test')]
+    public function test(Request $request): StreamedResponse
+    {
+        $message = $request->getContent();
+        return response()->eventStream(function () use ($message) {
+            yield $message;
+            $msgData = json_decode($message, true);
+            $stream = OpenAI::chat()->createStreamed([
+                'model' => 'deepseek-chat',
+                'messages' => [
+                    ['role' => 'user', 'content' => $msgData['message']],
+                ]
+            ]);
+            foreach ($stream as $response) {
+                yield $response->choices[0];
+            }
+        }, endStreamWith: new StreamedEvent(event: 'update', data: 'end'));
     }
 }
---
+++ web/src/pages/Online/index.tsx
@@ -0,0 +1,418 @@
+import {
+  Attachments,
+  Bubble,
+  Conversations,
+  Prompts,
+  Sender,
+  Welcome,
+  useXAgent,
+  useXChat, XStream,
+} from '@ant-design/x';
+import { createStyles } from 'antd-style';
+import React, { useEffect } from 'react';
+
+import {
+  CloudUploadOutlined,
+  CommentOutlined,
+  EllipsisOutlined,
+  FireOutlined,
+  HeartOutlined,
+  PaperClipOutlined,
+  PlusOutlined,
+  ReadOutlined,
+  ShareAltOutlined,
+  SmileOutlined, UserOutlined,
+} from '@ant-design/icons';
+import { Avatar, Badge, Button, type GetProp, Space } from 'antd';
+import { useModel } from '@umijs/max';
+
+const renderTitle = (icon: React.ReactElement, title: string) => (
+  <Space align="start">
+    {icon}
+    <span>{title}</span>
+  </Space>
+);
+
+const defaultConversationsItems = [
+  {
+    key: '0',
+    label: 'What is Ant Design X?',
+  },
+];
+
+const useStyle = createStyles(({ token, css }) => {
+  return {
+    layout: css`
+        width: 100%;
+        min-width: 1000px;
+        height: 722px;
+        border-radius: ${token.borderRadius}px;
+        border: 1px solid ${token.colorBorder};
+        display: flex;
+        background: ${token.colorBgContainer};
+        font-family: AlibabaPuHuiTi, ${token.fontFamily}, sans-serif;
+        
+        .ant-prompts {
+            color: ${token.colorText};
+        }
+    `,
+    menu: css`
+        background: ${token.colorBgLayout}80;
+        width: 280px;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    `,
+    conversations: css`
+        padding: 0 12px;
+        flex: 1;
+        overflow-y: auto;
+    `,
+    chat: css`
+        height: 100%;
+        width: 100%;
+        margin: 0 auto;
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+        padding: ${token.paddingLG}px;
+        gap: 16px;
+    `,
+    messages: css`
+        flex: 1;
+    `,
+    placeholder: css`
+        padding-top: 32px;
+    `,
+    sender: css`
+        box-shadow: ${token.boxShadow};
+    `,
+    logo: css`
+        display: flex;
+        height: 72px;
+        align-items: center;
+        justify-content: start;
+        padding: 0 24px;
+        box-sizing: border-box;
+
+        img {
+            width: 24px;
+            height: 24px;
+            display: inline-block;
+        }
+
+        span {
+            display: inline-block;
+            margin: 0 8px;
+            font-weight: bold;
+            color: ${token.colorText};
+            font-size: 16px;
+        }
+    `,
+    addBtn: css`
+        background: #1677ff0f;
+        border: 1px solid #1677ff34;
+        width: calc(100% - 24px);
+        margin: 0 12px 24px 12px;
+    `,
+  };
+});
+
+const placeholderPromptsItems: GetProp<typeof Prompts, 'items'> = [
+  {
+    key: '1',
+    label: renderTitle(<FireOutlined style={{ color: '#FF4D4F' }} />, 'Hot Topics'),
+    description: 'What are you interested in?',
+    children: [
+      {
+        key: '1-1',
+        description: `What's new in X?`,
+      },
+      {
+        key: '1-2',
+        description: `What's AGI?`,
+      },
+      {
+        key: '1-3',
+        description: `Where is the doc?`,
+      },
+    ],
+  },
+  {
+    key: '2',
+    label: renderTitle(<ReadOutlined style={{ color: '#1890FF' }} />, 'Design Guide'),
+    description: 'How to design a good product?',
+    children: [
+      {
+        key: '2-1',
+        icon: <HeartOutlined />,
+        description: `Know the well`,
+      },
+      {
+        key: '2-2',
+        icon: <SmileOutlined />,
+        description: `Set the AI role`,
+      },
+      {
+        key: '2-3',
+        icon: <CommentOutlined />,
+        description: `Express the feeling`,
+      },
+    ],
+  },
+];
+
+const senderPromptsItems: GetProp<typeof Prompts, 'items'> = [
+  {
+    key: '1',
+    description: 'Hot Topics',
+    icon: <FireOutlined style={{ color: '#FF4D4F' }} />,
+  },
+  {
+    key: '2',
+    description: 'Design Guide',
+    icon: <ReadOutlined style={{ color: '#1890FF' }} />,
+  },
+];
+
+
+const Independent: React.FC = () => {
+  // ==================== Style ====================
+  const { styles } = useStyle();
+  const userInfo = useModel('userModel', ({userInfo}) => userInfo);
+
+  const roles: GetProp<typeof Bubble.List, 'roles'> = {
+    ai: {
+      placement: 'start',
+      typing: { step: 5, interval: 20 },
+      avatar: { icon: <UserOutlined />, style: {color: '#f56a00', backgroundColor: '#fde3cf',} },
+      styles: {
+        content: {
+          borderRadius: 16,
+        },
+      },
+      header: "DeepSeek"
+    },
+    local: {
+      avatar: { icon: <Avatar src={userInfo?.avatar_url} style={{marginRight: '10px'}} /> },
+      placement: 'end',
+      variant: 'shadow',
+      header: userInfo?.nickname
+    },
+  };
+
+
+  // ==================== State ====================
+  const [headerOpen, setHeaderOpen] = React.useState(false);
+
+  const [content, setContent] = React.useState('');
+
+  const [conversationsItems, setConversationsItems] = React.useState(defaultConversationsItems);
+
+  const [activeKey, setActiveKey] = React.useState(defaultConversationsItems[0].key);
+
+  const [attachedFiles, setAttachedFiles] = React.useState<GetProp<typeof Attachments, 'items'>>(
+    [],
+  );
+
+  // ==================== Runtime ====================
+  const [agent] = useXAgent({
+    request: async ({ message }, { onSuccess, onUpdate }) => {
+      const response = await fetch(process.env.DOMAIN + "admin/test", {
+        "headers": {
+          "accept": "*/*",
+          "content-type": "text/plain;charset=UTF-8",
+        },
+        "body": JSON.stringify({message}),
+        "method": "POST",
+      });
+      if(!response.body) return;
+      let currentContent  = '';
+      for await (const chunk of XStream({
+        readableStream: response.body,
+      })) {
+        if(!chunk || !chunk.data || typeof chunk.data !== 'string') continue;
+        let currentMessage = chunk.data.replace(/\s*/g, "");
+        if (currentMessage == 'end') {
+          onSuccess(currentContent)
+        } else {
+          let data = JSON.parse(currentMessage);
+          if(data && data.delta && data.delta.content) {
+            currentContent = currentContent + data.delta.content;
+            onUpdate(currentContent)
+          }
+        }
+      }
+    },
+  });
+
+  const { onRequest, messages, setMessages } = useXChat({
+    agent,
+  });
+
+  useEffect(() => {
+    if (activeKey !== undefined) {
+      setMessages([]);
+    }
+  }, [activeKey]);
+
+  // ==================== Event ====================
+  const onSubmit = (nextContent: string) => {
+    if (!nextContent) return;
+    onRequest(nextContent);
+    setContent('');
+  };
+
+  const onPromptsItemClick: GetProp<typeof Prompts, 'onItemClick'> = (info) => {
+    onRequest(info.data.description as string);
+  };
+
+  const onAddConversation = () => {
+    setConversationsItems([
+      ...conversationsItems,
+      {
+        key: `${conversationsItems.length}`,
+        label: `New Conversation ${conversationsItems.length}`,
+      },
+    ]);
+    setActiveKey(`${conversationsItems.length}`);
+  };
+
+  const onConversationClick: GetProp<typeof Conversations, 'onActiveChange'> = (key) => {
+    setActiveKey(key);
+  };
+
+  const handleFileChange: GetProp<typeof Attachments, 'onChange'> = (info) =>
+    setAttachedFiles(info.fileList);
+
+  // ==================== Nodes ====================
+  const placeholderNode = (
+    <Space direction="vertical" size={16} className={styles.placeholder}>
+      <Welcome
+        variant="borderless"
+        icon="https://mdn.alipayobjects.com/huamei_iwk9zp/afts/img/A*s5sNRo5LjfQAAAAAAAAAAAAADgCCAQ/fmt.webp"
+        title="你好, 我是 Ant Design X"
+        description="基于 Ant Design，AGI 产品接口解决方案，打造更好的智能视觉~"
+        extra={
+          <Space>
+            <Button icon={<ShareAltOutlined />} />
+            <Button icon={<EllipsisOutlined />} />
+          </Space>
+        }
+      />
+      <Prompts
+        title="Do you want?"
+        items={placeholderPromptsItems}
+        styles={{
+          list: {
+            width: '100%',
+          },
+          item: {
+            flex: 1,
+          },
+        }}
+        onItemClick={onPromptsItemClick}
+      />
+    </Space>
+  );
+
+  const items: GetProp<typeof Bubble.List, 'items'> = messages.map(({ id, message, status }) => ({
+    key: id,
+    loading: status === 'loading',
+    role: status === 'local' ? 'local' : 'ai',
+    content: message,
+  }));
+
+  const attachmentsNode = (
+    <Badge dot={attachedFiles.length > 0 && !headerOpen}>
+      <Button type="text" icon={<PaperClipOutlined />} onClick={() => setHeaderOpen(!headerOpen)} />
+    </Badge>
+  );
+
+  const senderHeader = (
+    <Sender.Header
+      title="Attachments"
+      open={headerOpen}
+      onOpenChange={setHeaderOpen}
+      styles={{
+        content: {
+          padding: 0,
+        },
+      }}
+    >
+      <Attachments
+        beforeUpload={() => false}
+        items={attachedFiles}
+        onChange={handleFileChange}
+        placeholder={(type) =>
+          type === 'drop'
+            ? { title: 'Drop file here' }
+            : {
+              icon: <CloudUploadOutlined />,
+              title: 'Upload files',
+              description: 'Click or drag files to this area to upload',
+            }
+        }
+      />
+    </Sender.Header>
+  );
+
+  const logoNode = (
+    <div className={styles.logo}>
+      <img
+        src="https://file.xinadmin.cn/file/favicons.ico"
+        draggable={false}
+        alt="logo"
+      />
+      <span>Xin Admin AI</span>
+    </div>
+  );
+
+  // ==================== Render =================
+  return (
+    <div className={styles.layout}>
+      <div className={styles.menu}>
+        {/* 🌟 Logo */}
+        {logoNode}
+        {/* 🌟 添加会话 */}
+        <Button
+          onClick={onAddConversation}
+          type="link"
+          className={styles.addBtn}
+          icon={<PlusOutlined />}
+        >
+          New Conversation
+        </Button>
+        {/* 🌟 会话管理 */}
+        <Conversations
+          items={conversationsItems}
+          className={styles.conversations}
+          activeKey={activeKey}
+          onActiveChange={onConversationClick}
+        />
+      </div>
+      <div className={styles.chat}>
+        {/* 🌟 消息列表 */}
+        <Bubble.List
+          items={items.length > 0 ? items : [{ content: placeholderNode, variant: 'borderless' }]}
+          roles={roles}
+          className={styles.messages}
+        />
+        {/* 🌟 提示词 */}
+        <Prompts items={senderPromptsItems} onItemClick={onPromptsItemClick} />
+        {/* 🌟 输入框 */}
+        <Sender
+          value={content}
+          header={senderHeader}
+          onSubmit={onSubmit}
+          onChange={setContent}
+          prefix={attachmentsNode}
+          loading={agent.isRequesting()}
+          className={styles.sender}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Independent;
