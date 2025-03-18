db6c81e1278180e511eac23be453639b8d51cfc8
liu <2302563948@qq.com>
2025-03-18 16:31:15 +0800

    🐛fix(app): 修复登录页面加载失败的bug

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,4 +1,4 @@
-import React, { lazy } from 'react';
+import React from 'react';
 import { Navigate, RuntimeConfig, RunTimeLayoutConfig } from '@umijs/max';
 import settings from '@/default/settings';
 import defaultConfig from '@/utils/request';
@@ -11,6 +11,7 @@ import headerTitleRender from '@/components/Layout/HeaderTitleRender';
 import headerContentRender from '@/components/Layout/HeaderContentRender';
 import menuRender from '@/components/Layout/MenuRender';
 import PageAccess from '@/components/Access/PageAccess';
+import Login from '@/pages/Admin/components/Login';
 import './app.less';
 
 // 全局初始化状态
@@ -50,23 +51,17 @@ export const onRouteChange: RuntimeConfig['onRouteChange'] = ({location}) => {
 
 // 修改被 react-router 渲染前的树状路由表，接收内容同 useRoutes
 export const patchClientRoutes: RuntimeConfig['patchClientRoutes'] = ({ routes }) => {
-  const lazyLoad = (moduleName: string) => {
-    const Module = lazy(() => import(`./pages/${moduleName}`));
-    return <Module />;
-  };
   routes.unshift({
     path: '/',
     element: <Navigate to="/dashboard/analysis" replace />,
   });
-  routes.push([
-    {
-      name: '登录',
-      path: '/login',
-      id: 'adminLogin',
-      element: lazyLoad('Admin/components/Login'),
-      layout: false,
-    },
-  ]);
+  routes.unshift({
+    name: '登录',
+    path: '/login',
+    id: 'login',
+    element: <Login />  ,
+    layout: false,
+  });
 };
 
 // request 配置
