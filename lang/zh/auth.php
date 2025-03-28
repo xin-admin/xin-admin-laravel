e978b64af5e74129386b6da9b64ceba72ea25156
liu <2302563948@qq.com>
2025-03-28 15:36:18 +0800

    feat(auth): 添加 Sanctum 认证支持

---
+++ lang/zh/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '锁提供的凭据不匹配我们的记录。',
+    'password' => '输入的密码不正确。',
+    'throttle' => '登录尝试次数过多。请在：秒后重试。',
+
+];
---
+++ lang/zh/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => '您的密码已重置。',
+    'sent' => '我们已经通过电子邮件发送了您的密码重置链接。',
+    'throttled' => '请稍候再试。',
+    'token' => '此密码重置令牌无效。',
+    'user' => "我们找不到有那个邮箱地址的用户。",
+
+];
