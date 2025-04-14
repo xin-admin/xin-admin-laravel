ce96a9fe8b69b278f024c7c5d7e98920adb00ce1
liu <2302563948@qq.com>
2025-04-14 10:01:47 +0800

    feat: 添加跨域请求的头信息

--- app/Exceptions/ExceptionsHandler.php
+++ app/Exceptions/ExceptionsHandler.php
@@ -94,6 +94,7 @@ class ExceptionsHandler extends ExceptionHandler
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
         $response->headers->set('Access-Control-Max-Age', 1800);
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
 
         return $response;
     }
--- app/Middleware/AllowCrossDomain.php
+++ app/Middleware/AllowCrossDomain.php
@@ -22,6 +22,7 @@ class AllowCrossDomain
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
         $response->headers->set('Access-Control-Max-Age', 1800);
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
 
         // 如果是预检请求, 返回 204
         if ($request->isMethod('OPTIONS')) {
